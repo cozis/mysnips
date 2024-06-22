489ac917951a2239e14ca3912cbbc4f7e3dfa1db
Francesco Cozzuto <francesco.cozzuto@gmail.com>
2024-06-22 22:14:42 +0200

    Add asyncio again (but not as a submodule)

---
+++ asyncio/example.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "io.h"
+
+#define NUM_OPS 3
+
+int main()
+{
+    io_global_init();
+
+    struct io_operation ops[100];
+    struct io_resource  res[100];
+    struct io_context ioc;
+    if (!io_init(&ioc, res, ops, sizeof(res)/sizeof(res[0]), sizeof(ops)/sizeof(ops[0])))
+        return -1;
+
+    io_handle files[NUM_OPS];
+
+    for (int i = 0; i < NUM_OPS; i++) {
+        char name[1<<8];
+        snprintf(name, sizeof(name), "file_%d.txt", i);
+        files[i] = io_create_file(&ioc, name, IO_CREATE_CANTEXIST);
+        if (files[i] == IO_INVALID)
+            fprintf(stderr, "Couldn't create '%s'\n", name);
+    }
+
+    int started = 0;
+    char msg[] = "Hello, world!\n";
+    for (int i = 0; i < NUM_OPS; i++) {
+        if (io_send(&ioc, NULL, files[i], msg, sizeof(msg)-1))
+            started++;
+        else
+            fprintf(stderr, "ERROR\n");
+    }
+
+    for (int i = 0; i < started; i++) {
+        struct io_event ev;
+        io_wait(&ioc, &ev);
+        fprintf(stderr, "CONCLUDED\n");
+    }
+
+    io_free(&ioc);
+    io_global_free();
+    return 0;
+}
---
+++ asyncio/example2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <assert.h>
+#include "io.h"
+
+#define MAX_RES 100
+#define MAX_OPS 100
+
+int main(void)
+{
+    int port = 8080;
+
+    fprintf(stderr, "port=%d\n", port);
+
+    struct io_operation ops[MAX_OPS];
+    struct io_resource  res[MAX_RES];
+    struct io_context ioc;
+    
+    if (!io_global_init()) {
+        fprintf(stderr, "Couldn't perform the global initialization\n");
+        return -1;
+    }
+
+    if (!io_init(&ioc, res, ops, MAX_RES, MAX_OPS)) {
+        fprintf(stderr, "Couldn't initialize I/O context\n");
+        return -1;
+    }
+
+    io_handle socket = io_start_server(&ioc, NULL, port);
+    if (socket == IO_INVALID) {
+        fprintf(stderr, "Couldn't start listening\n");
+        return -1;
+    }
+    if (!io_accept(&ioc, NULL, socket)) {
+        fprintf(stderr, "Couldn't start accept operation\n");
+        return -1;
+    }
+
+    for (;;) {
+        struct io_event ev;
+        io_wait(&ioc, &ev);
+
+        assert(ev.optype == IO_ACCEPT);
+        io_handle accepted = ev.accepted;
+
+        fprintf(stderr, "Accepted\n");
+
+        io_close(&ioc, accepted);
+
+        if (!io_accept(&ioc, NULL, socket)) {
+            fprintf(stderr, "Couldn't start accept operation\n");
+            return -1;
+        }
+    }
+
+    io_free(&ioc);
+    io_global_free();
+    return 0;
+}
